09312ce9ab1282844bbec1e435cac03787b02aeb
Popcorn245 <bobby@madnesslabs.net>
2022-02-04 22:01:47 -0600

    fix(): Add settings files and connect file

---
+++ src/connect.ts
@@ -0,0 +1,44 @@
+import admin from "https://cdn.skypack.dev/firebase-admin";
+import * as fireorm from "https://cdn.skypack.dev/fireorm";
+
+export default function connect(options?: {
+  serviceAccount?: string | boolean;
+  emulate?: boolean;
+  host?: string;
+  ssl?: string;
+  ignoreUndefinedProperties?: boolean;
+  projectId?: string;
+}) {
+  const appConfig: admin.AppOptions = {};
+  if (
+    !!options?.emulate ||
+    options?.projectId ||
+    process?.env?.GCLOUD_PROJECT
+  ) {
+    appConfig.projectId = options?.projectId || process.env.GCLOUD_PROJECT;
+  }
+  if (options?.serviceAccount) {
+    const serviceAccount = require(typeof options?.serviceAccount === "string"
+      ? options.serviceAccount
+      : `${process.cwd()}/service-account.json`);
+    appConfig.credential = admin.credential.cert(serviceAccount);
+    appConfig.databaseURL = `https://${serviceAccount.project_id}.firebaseio.com`;
+    appConfig.storageBucket = `${serviceAccount.project_id}.appspot.com`;
+  }
+  admin.initializeApp(appConfig);
+
+  const firestore = admin.firestore();
+  const firebaseConfig: FirebaseFirestore.Settings = {
+    ignoreUndefinedProperties:
+      options?.ignoreUndefinedProperties === false ? false : true,
+  };
+  if (options?.emulate) {
+    firebaseConfig.host = options?.host || "localhost:8080";
+    firebaseConfig.ssl = !!options?.ssl;
+  }
+
+  firestore.settings(firebaseConfig);
+  fireorm.initialize(firestore);
+
+  return firestore;
+}
