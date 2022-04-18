7071e8257fd141a0dfa02be14a1788f2dcaa4c88
Popcorn245 <bobby@madnesslabs.net>
2022-04-17 23:49:31 -0500

    fix(): Correct bug with template fetching

--- src/routes/getTemplate.ts
+++ src/routes/getTemplate.ts
@@ -1,5 +1,5 @@
 export default async function getTemplate({ db, response, params }: any) {
   if (!params?.templateId) throw new Error("No template found!");
-  const template = await db.find(params?.templateId);
+  const template = await db.find("templates", params?.templateId);
   return template?.html || "^_^";
 }
