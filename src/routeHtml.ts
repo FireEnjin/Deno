f56322a51e69197ae2efa458cae16aece42a9407
Popcorn245 <bobby@madnesslabs.net>
2022-04-17 23:42:10 -0500

    fix(): Correct issues with removing handlebars

--- src/routeHtml.ts
+++ src/routeHtml.ts
@@ -1,10 +1,9 @@
 export default function routeHtml(
-  handlebars: any,
   db: any,
   routeFn: (context: any) => Promise<any>
 ) {
   return async ({ response, params }: any) => {
     response.headers.set("Content-Type", "text/html; charset=utf-8");
-    response.body = await routeFn({ handlebars, db, response, params });
+    response.body = await routeFn({ db, response, params });
   };
 }
--- src/routes/getTemplate.ts
+++ src/routes/getTemplate.ts
@@ -1,4 +1,5 @@
 export default async function getTemplate({ db, response, params }: any) {
   if (!params?.templateId) throw new Error("No template found!");
-  return db.find(params?.templateId);
+  const template = await db.find(params?.templateId);
+  return template?.html || "^_^";
 }
